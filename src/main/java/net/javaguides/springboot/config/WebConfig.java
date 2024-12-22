81e4f1aa43f15a97f0a8b0ef4d6e5bd9654cf68a
Your Name <sairamlanka8458@gmail.com>
2024-12-22 13:06:56 +0530

    Changeing the Cros domina

--- src/main/java/net/javaguides/springboot/config/WebConfig.java
+++ src/main/java/net/javaguides/springboot/config/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://mernui.onrender.com")
+                .allowedOrigins("https://mernui-1.onrender.com")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
